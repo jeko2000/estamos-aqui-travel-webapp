769b4e33c8587d46676f2c35f32d6a870c798b95
Johnny Ruiz <jeko2000@yandex.com>
2017-10-16 20:54:15 -0400

    Add sticky footer fix to tag pages

--- src/eat/layout/user/tag.clj
+++ src/eat/layout/user/tag.clj
@@ -1,8 +1,9 @@
 (ns eat.layout.user.tag
-  (:require [eat.layout.user.base :refer [base previews]]))
+  (:require [eat.layout.user.base :refer [base previews]]
+            [eat.layout.components :refer [sticky-footer-fix]]))
 
- (defn tag-page [layout-config target-tag posts-with-tag]
+(defn tag-page [layout-config target-tag posts-with-tag]
   (base layout-config
         {:title (str "Posts under tag " target-tag)
          :content (previews layout-config posts-with-tag (str "Posts with tag " target-tag))
-         :posts posts-with-tag}))
+         :js (sticky-footer-fix)}))
