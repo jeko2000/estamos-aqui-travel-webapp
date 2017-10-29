6ebd22472dfc562a6ec4d646e375de0c05727780
Johnny Ruiz <jeko2000@yandex.com>
2017-10-29 19:40:17 -0400

    Add url and license keys to project.clj

--- project.clj
+++ project.clj
@@ -1,13 +1,16 @@
 (defproject eat "1.0.2"
   :description "The web application for http://estamosaquitravel.com"
+  :url "https://estamosaquitravel.com"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring "1.6.2"]
-                 [ring/ring-defaults "0.3.1"]                 
+                 [ring/ring-defaults "0.3.1"]
                  [compojure "1.6.0"]
                  [hiccup "1.0.5"]
                  [me.raynes/cegdown "0.1.1"]
                  [buddy "2.0.0"]
-                 [org.clojure/java.jdbc "0.7.3"]                 
+                 [org.clojure/java.jdbc "0.7.3"]
                  [org.postgresql/postgresql "42.1.4"]
                  [clj-http "3.7.0"]
                  [cheshire "5.8.0"]
