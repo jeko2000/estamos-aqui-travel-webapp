2691234a42e5d1b939bb14d8ae309405bd3a5de5
Johnny Ruiz <jeko2000@yandex.com>
2017-11-06 16:03:51 -0500

    Update api.clj to handle errors

--- src/clj/eat/api.clj
+++ src/clj/eat/api.clj
@@ -6,18 +6,20 @@
   (-> (:instagram-access-token @config)))
 
 (defn- get-recent-media-vector [token count]
-  (-> "https://api.instagram.com/v1/users/self/media/recent/?access_token=%s&count=%s"
-      (format token count)
-      (client/get {:as :json})
-      (get-in [:body :data])))
+  (try
+    (-> "https://api.instagram.com/v1/users/self/media/recent/?access_token=%s&count=%s"
+        (format token count)
+        (client/get {:as :json})
+        (get-in [:body :data]))
+    (catch Exception e [])))
 
-(defn- get-image-url-from-media-item [res-key {:keys [link images]}]
-  "The value for RES-KEY must be one of :thumbnail, :low_resolution, or :standard_resolution"
+(defn- get-image-url-from-media-item [resolution {:keys [link images]}]
+  "The value for RESOLUTION must be one of :thumbnail, :low_resolution, or :standard_resolution"
   {:url link
-   :image-res (get-in images [res-key :url])})
+   :image-res (get-in images [resolution :url])})
 
-(defn instagram-get-recent-images [count res-key]
+(defn instagram-get-recent-images [count resolution]
   (-> *instagram-access-token*
       (get-recent-media-vector count)
-      (->> (mapv #(get-image-url-from-media-item res-key %)))))
+      (->> (mapv #(get-image-url-from-media-item resolution %)))))
 
