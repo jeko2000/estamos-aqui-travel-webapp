76c80d1c46afb6c0bd2a6a8b2fe30168a2cda3de
Johnny Ruiz <jeko2000@yandex.com>
2017-11-06 15:21:00 -0500

    Update dependency versions

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring "1.6.2"]
+                 [ring "1.6.3"]
                  [ring/ring-defaults "0.3.1"]
                  [compojure "1.6.0"]
                  [hiccup "1.0.5"]
                  [me.raynes/cegdown "0.1.1"]
-                 [bouncer "1.0.0"]
+                 [bouncer "1.0.1"]
                  [buddy "2.0.0"]
                  [org.clojure/java.jdbc "0.7.3"]
                  [org.postgresql/postgresql "42.1.4"]
@@ -42,7 +42,7 @@
                    :dependencies [[org.clojure/clojurescript "1.9.946"]
                                   [com.cemerick/piggieback "0.2.2"]
                                   [figwheel-sidecar "0.5.14"]]
-                   :plugins [[lein-figwheel "0.5.13"]
+                   :plugins [[lein-figwheel "0.5.14"]
                              [lein-cljsbuild "1.1.1"]]
                    :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
                    :clean-targets ^{:protect false} ["resources/public/js"
